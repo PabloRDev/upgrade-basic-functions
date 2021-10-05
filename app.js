1983ac72a5055e1bab0a0a073446cc3b6826bb36
PabloRDev <pablord@MacBook-Air-de-Pablo.local>
2021-10-05 17:45:40 +0200

    ITERACION 3

--- app.js
+++ app.js
@@ -35,4 +35,19 @@ function findLongestWord(avengers) {
 console.log(findLongestWord(avengers));
 
 //Iteracion #3
+// Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+// Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz.
 
+// Puedes usar este array para probar tu función:
+let numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(numbers) {
+  let total = 0;
+  for (let index = 0; index < numbers.length; index++) {
+    total+=numbers[index];
+  }
+
+  return total;
+}
+
+console.log(sumAll(numbers));
