6bf4b7e615542319dd4d38b07b71c9232a727cfe
PabloRDev <pablord@MacBook-Air-de-Pablo.local>
2021-10-06 15:52:20 +0200

    ITERACION 7

--- app.js
+++ app.js
@@ -110,4 +110,33 @@ function removeDuplicates(param) {
 console.log("Duplicados", duplicates);
 console.log("No duplicados", removeDuplicates(duplicates));
 
+// Iteracion #7
+
+// Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+function finderName(param, name) {
+  for (let index = 0; index < param.length; index++) {
+    if (param[index] === name) {
+      return index;
+    }
+  }
+
+  return "No se encuentra el nombre";
+}
+
+console.log(finderName(nameFinder, "Paco"));
+
 
