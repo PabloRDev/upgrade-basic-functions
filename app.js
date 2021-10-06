53fa3f052244a325d283befaf7e38a36f28f018d
PabloRDev <pablord@MacBook-Air-de-Pablo.local>
2021-10-06 15:51:57 +0200

    ITERACION 6

--- app.js
+++ app.js
@@ -82,4 +82,32 @@ function averageWord(param) {
 }
 console.log(averageWord(mixedElements));
 
+// Iteracion #6
+// Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función:
+let duplicates = [
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
+function removeDuplicates(param) {
+  let newElements = [];
+  for (let index = 0; index < param.length; index++) {
+    if (!newElements.includes(param[index])) {
+      newElements.push(param[index]);
+    }
+  }
+  return newElements;
+}
+
+console.log("Duplicados", duplicates);
+console.log("No duplicados", removeDuplicates(duplicates));
+
 
